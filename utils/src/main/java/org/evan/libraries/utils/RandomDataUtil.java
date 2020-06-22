cfcd9da2a217d353b1738cef92dc84058efec9c0
沈炜 <277469513@qq.com>
2020-06-22 09:35:19 +0800

    随机数工具

--- utils/src/main/java/org/evan/libraries/utils/RandomDataUtil.java
+++ utils/src/main/java/org/evan/libraries/utils/RandomDataUtil.java
@@ -2,38 +2,35 @@ package org.evan.libraries.utils;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 随机数据生成工具类.
- *
  */
 public class RandomDataUtil {
-
-    private static Random random = new Random();
 
     /**
      * 返回随机ID.
      */
     public static long randomId() {
-        return random.nextLong();
+        return ThreadLocalRandom.current().nextLong();
     }
 
     public static double randomDouble() {
-        return random.nextDouble();
+        return ThreadLocalRandom.current().nextDouble();
     }
 
     public static int randomInt(int max) {
-        return random.nextInt(max);
+        return ThreadLocalRandom.current().nextInt(max);
     }
 
     public static long randomLong(int max) {
-        return random.nextInt(max);
+        return ThreadLocalRandom.current().nextInt(max);
     }
 
     public static String randomString(int length) {
         int max = (int) Math.pow(10, length);
-        int int1 = random.nextInt(max - 1);
+        int int1 = ThreadLocalRandom.current().nextInt(max - 1);
         return StringUtil.leftPad(int1 + "", length, "0");
     }
 
@@ -41,7 +38,7 @@ public class RandomDataUtil {
      * 返回随机名称, prefix字符串+5位随机数字.
      */
     public static String randomName(String prefix) {
-        return prefix + random.nextInt(10000);
+        return prefix + ThreadLocalRandom.current().nextInt(10000);
     }
 
     /**
@@ -64,7 +61,7 @@ public class RandomDataUtil {
      * 从输入list中随机返回随机个对象.
      */
     public static <T> List<T> randomSome(List<T> list) {
-        int size = random.nextInt(list.size());
+        int size = ThreadLocalRandom.current().nextInt(list.size());
         if (size == 0) {
             size = 1;
         }
